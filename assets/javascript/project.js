94ae070f2adf199b1285eecab8dcbbbbb79d2e9b
AfterTime <ywhangus@gmail.com>
2025-10-29 23:17:30 +0800

    forgot light mode js

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -293,4 +293,29 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         window.addEventListener('DOMContentLoaded', hideLoader);
     }
+
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+    if (themeToggle) {
+        const icon = themeToggle.querySelector('i');
+        if (localStorage.getItem('theme') === 'light') {
+            body.classList.add('light-mode');
+            icon.classList.remove('fa-sun');
+            icon.classList.add('fa-moon');
+        }
+
+        themeToggle.addEventListener('click', () => {
+            body.classList.toggle('light-mode');
+
+            if (body.classList.contains('light-mode')) {
+                localStorage.setItem('theme', 'light');
+                icon.classList.remove('fa-sun');
+                icon.classList.add('fa-moon');
+            } else {
+                localStorage.setItem('theme', 'dark');
+                icon.classList.remove('fa-moon');
+                icon.classList.add('fa-sun');
+            }
+        });
+    }
 });
